d676f905cabc0de8937a6d772410f981b8ddca5a
Michael Wert <mdwert@yog-sothoth.(none)>
2011-02-24 19:13:37 -0500

    Refactor

--- custom/test_theme.zsh
+++
@@ -1,4 +0,0 @@
-function test-theme {
-export ZSH_THEME="$(basename $1 .zsh-theme)"
-source $ZSH/oh-my-zsh.sh
-}
--- custom/vcs_info.zsh
+++
@@ -1,3 +0,0 @@
-# Directory based VCS before repo based ones (e.g. CVS in $HOME, the latter using Git)
-zstyle ':vcs_info:*' enable cvs svn git bzr hg
-zstyle ':vcs_info:bzr:*' use-simple true
